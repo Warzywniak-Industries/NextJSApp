a80c967e6fd7db6194a16f87384373ed8134c90c
Kaba <Kaba8081@gmail.com>
2024-09-14 21:12:08 +0200

    Style: gradient on hero slogan

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -6,7 +6,7 @@ export default function HeroBanner() {
     <div className="w-full flex flex-col sm:flex-row bg-background self-strech items-center justify-center px-[32px] py-16 gap-y-12 md:px-[11.25%]">
         <div>
             <h1 className="boldheader1 shrink-0 text-text">
-                "Transforming Ideas <br className="visible md:hidden"/> into Reality"
+                Transforming Ideas <br className="visible md:hidden"/> into <span className="bg-gradient-to-r from-primary to-secondary inline-block text-transparent bg-clip-text">Reality</span>
             </h1>
         </div>
         <div className="flex md:w-[32%] md:max-w-[432px] md:max-h-[464px]">
