6426564b8053334b45bf0ab79ee17e991a737d12
Kaba <Kaba8081@gmail.com>
2024-09-15 02:49:06 +0200

    Feat. Created a PostCard component

--- components/PostCarousel.tsx
+++ components/PostCarousel.tsx
@@ -2,16 +2,9 @@
 
 import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton"
+import { Skeleton } from "@/components/ui/skeleton";
+import PostCard from "@/components/PostCard";
 import Link from "next/link";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
 import {
   Carousel,
   CarouselContent,
@@ -39,38 +32,16 @@ export default function PostCarousel() {
             <div className="flex flex-row justify-center">
                 <Carousel className="w-[75%]">
                 <CarouselContent className="w-full">
-                    {posts.length===0 && [1,2,3,4,5].map((_, index) => (
+                    {posts.length===0 && [1,2,3,4,5,6,7].map((_, index) => (
                     <CarouselItem key={index} className="max-w-[350px]">
-                        <Card>
-                            <CardHeader>
-                                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-                                <Skeleton className="h-4 w-[250px]" />
-                            </CardHeader>
-                            <CardContent>
-                                <Skeleton className="h-4 w-[250px]" />
-                            </CardContent>
-                            <CardFooter>
-                                <Skeleton className="h-2 w-[250px]" />
-                            </CardFooter>
-                        </Card>
+                        <PostCard />
                     </CarouselItem>
                     ))}
-                    {/* {!(posts.length===0) && posts.map((post, index) => (
+                    {!(posts.length===0) && posts.map((post, index) => (
                     <CarouselItem key={index} className="max-w-[350px]">
-                        <Card>
-                            <CardHeader>
-                                <img src={post.thumbnail}/>
-                                <CardTitle>{post.title}</CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                                <p className="text-text opacity-70 basetext">{post.description}</p>
-                            </CardContent>
-                            <CardFooter>
-                                <p className="text-text opacity-50 footertext">{post.date}</p>
-                            </CardFooter>
-                        </Card>
+                        <PostCard post={post} />
                     </CarouselItem>
-                    ))} */}
+                    ))}
                 </CarouselContent>
                 <CarouselPrevious />
                 <CarouselNext />
--- types/Post.ts
+++ types/Post.ts
@@ -4,10 +4,12 @@ export type PostStatesType = typeof PostStates[number];
 
 export interface Post {
     slug: string;
+    thumbnail: string;
     title: string;
     description: string;
     content: string;
     state: PostStatesType;
+    date: string;
 }
 
 export interface PostOverview {
