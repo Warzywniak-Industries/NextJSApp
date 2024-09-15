b7b93cc5127014090319225db2512b1b91ffcd78
MrNtex <rtrniemiec@gmail.com>
2024-09-15 07:57:39 +0200

    chore: Add Avatar component to Navbar for user display

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -7,11 +7,24 @@ import Image from "next/image";
 import { NavLinks } from "@/constants/Navigation";
 import { Menu, Close } from "@/img";
 import { useAuth } from "@/context/AuthContext";
+import Avatar from "boring-avatars";
+
 
 const Navbar: React.FC = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
 
-    const { user } = useAuth()
+    const { user, userDataObj } = useAuth()
+
+    const UserLogged = () => {
+        if(!userDataObj) return
+        return (
+            <div className="flex h-12 items-center">
+                <h1 className="pr-3">Hi {userDataObj.firstName}</h1>
+                <Avatar name={userDataObj.firstName} className="h-[70%]"/>
+            </div>
+        )
+        
+    }
 
     return (
         <nav className="w-full py-4 bg-[#96c1e9]/30 shadow-text drop-shadow-lg items-center">
@@ -23,7 +36,8 @@ const Navbar: React.FC = () => {
                     <a href="/posts" className="border-2 p-2 rounded-none hover:bg-cyan-100 hover:rounded-xl transition-all ease-in-out duration-1000">
                         <h1>BROWSE</h1>
                     </a>
-                    <a href="/login"  className="font-extralight">Login / Sign Up</a>
+                    
+                    { user ? <UserLogged/> : <a href="/login"  className="font-extralight">Login / Sign Up</a>}
                 </div>
             </div>
         </nav>
